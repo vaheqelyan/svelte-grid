6f40ab921c304467337b7d539a56147dbc0ab3ea
vaheqelyan <martinkenner@mail.com>
2019-08-02 13:45:03 +0400

    Save changes

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,12 @@ import resolve from "rollup-plugin-node-resolve";
 import pkg from "./package.json";
 
 const name = pkg.name
-  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
-  .replace(/^\w/, m => m.toUpperCase())
-  .replace(/-\w/g, m => m[1].toUpperCase());
+	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
+	.replace(/^\w/, m => m.toUpperCase())
+	.replace(/-\w/g, m => m[1].toUpperCase());
 
 export default {
-  input: "src/index.svelte",
-  output: [{ file: pkg.module, format: "es" }, { file: pkg.main, format: "umd", name }],
-  plugins: [svelte(), resolve()],
+	input: "src/index.svelte",
+	output: [{ file: pkg.module, format: "es" }, { file: pkg.main, format: "umd", name }],
+	plugins: [svelte(), resolve()],
 };
