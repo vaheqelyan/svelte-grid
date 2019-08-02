dd48dba91d7e2b9f7335288d6ff7a7f0739bc38f
vaheqelyan <martinkenner@mail.com>
2019-08-02 20:22:13 +0400

    Create a helper file

---
+++ src/utils/helper.js
@@ -0,0 +1,38 @@
+import { findCloseBlocks, findItemsById, makeMatrixFromItemsIgnore, makeMatrixFromItems } from "../utils/matrix.js";
+import { getLastItemStats, findFreeSpaceForItem, resizeItems, moveItem } from "../utils/item.js";
+
+function makeItem(item) {
+  return {
+    ...item,
+    ...{
+      drag: { top: null, left: null, dragging: false },
+      resize: { width: null, height: null, resizing: false },
+      responsive: { valueW: 0, valueH: 0 },
+      min: {
+        ...item.min,
+      },
+      max: {
+        ...item.max,
+      },
+    },
+  };
+}
+
+const gridHelp = {
+  findSpaceForItem(item, items, cols) {
+    let matrix = makeMatrixFromItems(items, getLastItemStats(items), cols);
+
+    let position = findFreeSpaceForItem(matrix, item, items);
+    return position;
+  },
+
+  appendItem(item, items, cols) {
+    return moveItem(item, [...items, ...[item]], cols);
+  },
+
+  resizeItems(items, col, rows) {
+    return resizeItems(items, col, rows);
+  },
+};
+
+export { gridHelp, makeItem, getLastItemStats, resizeItems };
