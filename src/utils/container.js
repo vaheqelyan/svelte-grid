2924a0c0224817473f3d1c2ce77e8a005500b09e
vaheqelyan <martinkenner@mail.com>
2019-08-02 14:26:21 +0400

    Save changes

--- src/utils/container.js
+++ src/utils/container.js
@@ -1,5 +1,5 @@
 import { getClosestToRow } from "./item.js";
 
 export function getContainerHeight(items, yPerPx) {
-	return Math.max(getClosestToRow(items), 2) * yPerPx;
+  return Math.max(getClosestToRow(items), 2) * yPerPx;
 }
