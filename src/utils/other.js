2c54382d08142d355ac8ffac36017101201c4b4b
vaheqelyan <martinkenner@mail.com>
2019-08-04 14:17:09 +0400

    Pretty

--- src/utils/other.js
+++ src/utils/other.js
@@ -10,23 +10,24 @@ export function getLastItemStats(items) {
   return Math.max(...items.map(val => val.y + val.h), 1);
 }
 
-export const getColumnFromBreakpoints = (breakpoints,windowWidth, cols, initCols) => {
-      var found=false,tempCols=cols;
-    if(breakpoints) {
+export const getColumnFromBreakpoints = (breakpoints, windowWidth, cols, initCols) => {
+  var found = false,
+    tempCols = cols;
+  if (breakpoints) {
+    for (var i = breakpoints.length - 1; i >= 0; i--) {
+      const [resolution, cols] = breakpoints[i];
 
-      for (var i = breakpoints.length - 1; i >= 0; i--) {
-      const [resolution, cols] = breakpoints[i]
-
-      if(windowWidth <= resolution) {
-        found = true
-        tempCols=cols
+      if (windowWidth <= resolution) {
+        found = true;
+        tempCols = cols;
         break;
       }
     }
   }
 
-    if(!found) { tempCols=initCols }
+  if (!found) {
+    tempCols = initCols;
+  }
 
-      return tempCols
-
-}
+  return tempCols;
+};
