4d15b18a41fcae150e14d5abd6f035d3242306ae
vaheqelyan <martinkenner@mail.com>
2019-08-04 13:35:07 +0400

    Export function

--- src/utils/other.js
+++ src/utils/other.js
@@ -9,3 +9,24 @@ export const debounce = (fn, ms = 0) => {
 export function getLastItemStats(items) {
   return Math.max(...items.map(val => val.y + val.h), 1);
 }
+
+export const getColumnFromBreakpoints = (breakpoints,windowWidth, cols, initCols) => {
+      var found=false,tempCols=cols;
+    if(breakpoints) {
+
+      for (var i = breakpoints.length - 1; i >= 0; i--) {
+      const [resolution, cols] = breakpoints[i]
+
+      if(windowWidth <= resolution) {
+        found = true
+        tempCols=cols
+        break;
+      }
+    }
+  }
+
+    if(!found) { tempCols=initCols }
+
+      return tempCols
+
+}
