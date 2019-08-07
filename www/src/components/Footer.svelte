bc6828d18b95967d962515fbcb27abf551c2fa9b
vaheqelyan <martinkenner@mail.com>
2019-08-07 11:33:37 +0400

    Create footer

---
+++ www/src/components/Footer.svelte
@@ -0,0 +1,10 @@
+<style>
+	.footer {
+		padding: 10px 10px;
+		text-align: center;
+	}
+</style>
+
+<div class=footer>
+	Created by <a target=_blank href=https://github.com/vaheqelyan>@vaheqelyan</a> for awesome svelte.js ❤️ 
+</div>
