630a5b7dffd4115128b9035ef1f1bf1fc0234035
vaheqelyan <martinkenner@mail.com>
2019-08-07 11:36:41 +0400

    Create page

---
+++ www/src/routes/examples/index.svelte
@@ -0,0 +1,21 @@
+<style>
+	.list {
+		display: flex;
+		flex-direction: column;
+	}
+</style>
+
+<h1>Examples</h1>
+
+<svelte:head>
+  <title>Svelte-grid — Examples</title>
+</svelte:head>
+
+<div class=list>
+	<a href=examples/basic>Basic</a>
+	<a href=examples/static>Static</a>
+	<a href=examples/gap>Gap</a>
+	<a href=examples/add_remove>Add/Remove</a>
+	<a href=examples/responsive>Responsive</a>
+	<a href=examples/min_max>Min/Max resize</a>
+</div>
