94dd2659bd42cdfee7742ca0226245be7fc8047c
vaheqelyan <martinkenner@mail.com>
2019-08-07 14:23:40 +0400

    Made less clear documentation.

--- www/src/routes/usage.svelte
+++ www/src/routes/usage.svelte
@@ -28,22 +28,72 @@ tr:nth-child(even) {
 
 <p>Use <b>Grid</b> like any other component.</p>
 
+<p>The basic example</p>
+
+<code>
+  <pre>{`
+    <script>
+      import Grid from 'svelte-grid'
+      import gridHelp from 'svelte-grid/build/helper'
+
+      let items = [gridHelp.item({x:0,y:0,h:1,w:1, id:'unique_id'})]
+    </script>
+    <Grid items={items} cols={3} bind:items={items} let:item={item}>
+      <div>{item.id}</div>
+    </Grid>
+  `}</pre>
+</code>
+
+<p><a href=examples>See more examples</a></p>
+
+<hr/>
+
+<h2>How to style</h2>
+
+<p>Right now this is a problem for sveltejs</p>
+
+<p>But you can apply styles to a selector globally, use the <b>:global(...)</b> modifier.</p>
+
+<p>For example</p>
+
+<code>
+  <pre>
+    {`
+<style>
+  :global(.svlt-grid-container) {
+    background: red;
+  }
+</style>`}
+  </pre>
+</code>
+
+<p>Here are the class names</p>
+
+<ul>
+  <li><code>.svlt-grid-container</code> — Class name for container grid</li>
+  <li><code>.svlt-grid-item</code> — Slot shell class name</li>
+  <li><code>.svlt-grid-shadow</code> — Class name for the back shadow when resizing or moving an element</li>
+  <li><code>.svlt-grid-resizer</code> — Class name for resizer</li>
+  <li><code>.svlt-grid-resizer::after</code> — Class name for resizer</li>
+</ul>
+
+
 <h2>Helper functions</h2>
 
 <hr/>
 
-<p>You can import helper functions from <code>svelte-grid/build/helper/index.mjs</code></p>
+<p>The helper function is located in <code>svelte-grid/build/helper/index.mjs</code></p>
 
 <b>Sample</b>
 <code>
-	<pre>{`import {gridHelp} from 'svelte-grid/build/helper/index.mjs'`}</pre>
+	<pre>{`import gridHelp from 'svelte-grid/build/helper'`}</pre>
 </code>
 
 <h3>What's in there?</h3>
 
 <p>Well there are several functions that will help you manage the grid elements</p>
 
-<b><code><pre>{`makeItem(item: Object)`}</pre></code></b>
+<b><code><pre>{`gridHelp.item(item: Object)`}</pre></code></b>
 
 <p>Returns an object with the properties necessary to create a widget</p>
 
@@ -78,13 +128,33 @@ tr:nth-child(even) {
 
 <b><code><pre>{`gridHelp.findSpaceForItem(item: Object, items: Array<Object>, cols: Number)`}</pre></code></b>
 
+<p>Find out where to place the widget in the grid by calculating from left to right returns an object of two properties x and y.<code>{`{ x: Number, y: Number}`}</code></p>
+<p>You need to assign the returned result and then only concat it with an array of objects</p>
+<p><a href=examples/add_remove>See example</a></p>
+
 <hr/>
 
 <b><code><pre>{`gridHelp.appendItem(item: Object, items: Array<Object>, cols: Number)`}</pre></code></b>
 
+<p>Returns an array of objects, including an item that should have been added to your grid.</p>
+
+<p>Suppose you want to add an element at a specific position in the grid. But what if the position is not empty and the intended element covers other widgets in the grid.</p>
+<p>You need to find another place for them too.</p>
+
+<p>This function will find out if the newly inserted widget covers other widgets. If so, it will find a place for them and return an array of objects</p>
+
+<p><a href=examples/add_remove>See example</a></p>
+
 <hr/>
 
-<b><code><pre>{`gridHelp.findSpaceForItem(item: Object, cols: Number, rows: Number)`}</pre></code></b>
+<b><code><pre>{`gridHelp.resizeItems(item: Object, cols: Number, rows: Number)`}</pre></code></b>
+
+<p>Returns the array of objects</p>
+
+<p>This is mainly used to keep your grid responsive.</p>
+<p>It will find a new location and resize the widget based on the column size</p>
+
+<p><a href=https://github.com/vaheqelyan/svelte-grid/blob/master/www/src/routes/index.svelte target=_blank>See example</a></p>
 
 <hr/>
 
