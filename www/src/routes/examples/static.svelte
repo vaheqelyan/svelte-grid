cd08f8ab4bd772ce003eb12256a85fd0d2937388
vaheqelyan <martinkenner@mail.com>
2019-08-07 11:35:28 +0400

    Create static sample

---
+++ www/src/routes/examples/static.svelte
@@ -0,0 +1,68 @@
+<style>
+	.content {
+		width:100%;
+		height: 100%;
+		color: black;
+		display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: large;
+	}
+	:global(.svlt-grid-shadow) {
+    background: pink;
+  }
+  :global(.svlt-grid-container){
+  	background: #eee;
+  }
+  .link {
+  	display: flex;
+  	padding-bottom: 20px;
+  }
+  .link a {
+  	margin-left: 10px;
+  }
+</style>
+
+<script>
+	import Grid from 'svelte-grid/src/index.svelte'
+	import {makeItem} from 'svelte-grid/build/helper/index.mjs'
+  import {openInSource} from './github.js'
+
+
+	const id = () =>
+  "_" +
+  Math.random()
+    .toString(36)
+    .substr(2, 9);
+
+	let items = [
+  makeItem({x:0,y:0,w:2,h:2,id:id()}),
+  makeItem({x:2,y:0,w:3,h:2,id:id(), static:true}),
+  makeItem({x:5,y:0,w:3,h:3,id:id()})
+  ]
+
+</script>
+
+<svelte:head>
+  <title>Example — Static</title>
+    <meta name="description" content="Svelte-grid — Example — Static"/>
+  <meta name="keywords" content="draggable,resizable,grid,layout,responsive,breakpoints,Svelte,svelte,svelte.js,sveltejs,usage,example,examples,samples,static"/>
+  <meta name="author" content="Vahe Araqelyan"/>
+</svelte:head>
+
+
+<h1>Static</h1>
+
+<p><a href=examples/gap>→ Gap</a></p>
+<p><a href=examples/static>← Basic</a></p>
+
+<p>This demo sets an item to static. Static elements cannot be moved or resized. Other elements move themselves around a static element.</p>
+
+<div class=link>
+	<a href={`${openInSource}/static.svelte`} target=_blank>View this example's source</a>
+	<a href="https://svelte.dev/repl/9023ed6cd43540bc995e1c84fbc90836?version=3.7.1" target=_blank>Open in repl</a>
+</div>
+
+<Grid {items} bind:items={items} cols={8} let:item={item} rowHeight={100}>
+	<div class=content style="background: {item.static ? '#cce' : '#ccc'};border:1px solid black;">{item.id}</div>
+</Grid>
