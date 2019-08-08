e22b08f6660811993ba7c977c8d24e6e426cadd1
vaheqelyan <martinkenner@mail.com>
2019-08-08 16:43:32 +0400

    Save changes

--- www/src/routes/examples/index.svelte
+++ www/src/routes/examples/index.svelte
@@ -1,7 +1,31 @@
 <style>
-	.list {
-		display: flex;
-		flex-direction: column;
+	.sample {
+		width: 100%;
+		height: 100%;
+		background: white;
+		border: 1px solid rgba(147, 128, 108, 0.25);
+	}
+	video {
+		max-width: 100%;
+		width: 100%;
+		height: 80%;
+	}
+	.text-block {
+		padding:2% 4%;
+		height: 20%;
+	}
+	
+	:global(.svlt-grid-container){
+		background: white!important;
+	}
+
+	:global(.svlt-grid-shadow){
+		background: pink;
+	}
+
+	h3 {
+		text-transform:capitalize;
+		text-decoration: overline;	
 	}
 </style>
 
@@ -11,11 +35,36 @@
   <title>Svelte-grid — Examples</title>
 </svelte:head>
 
-<div class=list>
-	<a href=examples/basic>Basic</a>
-	<a href=examples/static>Static</a>
-	<a href=examples/gap>Gap</a>
-	<a href=examples/add_remove>Add/Remove</a>
-	<a href=examples/responsive>Responsive</a>
-	<a href=examples/min_max>Min/Max resize</a>
-</div>
+
+<Grid {items} bind:items={items} {cols} let:item={item} gap={10} {breakpoints}>
+	<div class=sample>
+		<video controls={false} autoplay loop>
+	  <source src={`${cloudinary}/${item.id}.webm`} type="video/webm">
+	  Your browser does not support the video tag.
+	</video>
+		<div class=text-block>
+			<h3><a href={`examples/${item.id}`}>{item.id} example</a></h3>
+		</div>
+	</div>	
+</Grid>
+
+<script>
+	import Grid from 'svelte-grid'
+	import gridHelp from 'svelte-grid/build/helper'
+
+const cloudinary = 'https://res.cloudinary.com/dmtrk3yns/video/upload/v1565264291/svelte-grid'
+
+	let items = [
+		gridHelp.item({x:0,y:0,w:2,h:2,id:'basic', resizable: false}),
+		gridHelp.item({x:2,y:0,w:2,h:2,id:'static', resizable: false}),
+		gridHelp.item({x:0,y:2,w:2,h:2,id:'gap', resizable: false}),
+		gridHelp.item({x:2,y:2,w:2,h:2,id:'add_remove', resizable: false}),
+		gridHelp.item({x:0,y:4,w:2,h:2,id:'responsive', resizable: false}),
+		gridHelp.item({x:2,y:4,w:2,h:2,id:'min_max', resizable: false}),
+	]
+	let cols = 4;
+
+	const breakpoints = [
+		[740, 1]
+	]
+</script>
