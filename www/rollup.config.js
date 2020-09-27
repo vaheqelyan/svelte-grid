0e5e7cfb287f9ff99e1b7bb1fc8b46495e0a60e4
Vahe Araqelyan <vahe.araqelyan.2013@hotmail.com>
2020-09-27 11:13:13 -0400

    refactor: mdsvex integration

--- www/rollup.config.js
+++ www/rollup.config.js
@@ -1,82 +1,107 @@
-import resolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
-import commonjs from 'rollup-plugin-commonjs';
-import svelte from 'rollup-plugin-svelte';
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import config from 'sapper/config/rollup.js';
-import pkg from './package.json';
+import 'prismjs';
+import "prism-svelte";
+
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import commonjs from "@rollup/plugin-commonjs";
+import svelte from "rollup-plugin-svelte";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
+import config from "sapper/config/rollup.js";
+import pkg from "./package.json";
+import { mdsvex } from "mdsvex";
+import glob from 'rollup-plugin-glob'
 
 const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
+const dev = mode === "development";
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
-const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && warning.message.includes('/@sapper/')) || onwarn(warning);
+const onwarn = (warning, onwarn) =>
+	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
+	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
+	onwarn(warning);
 
+const extensions = [".svelte", ".svx"];
 export default {
 	client: {
 		input: config.client.input(),
 		output: config.client.output(),
 		plugins: [
+		glob(),
 			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				"process.browser": true,
+				"process.env.NODE_ENV": JSON.stringify(mode)
 			}),
 			svelte({
+				extensions,
 				dev,
 				hydratable: true,
-				emitCss: true
+				emitCss: true,
+				preprocess: mdsvex()
 			}),
 			resolve({
-				browser: true
+				browser: true,
+				dedupe: ['svelte']
 			}),
 			commonjs(),
 
-			legacy && babel({
-				extensions: ['.js', '.mjs', '.html', '.svelte'],
-				runtimeHelpers: true,
-				exclude: ['node_modules/@babel/**'],
-				presets: [
-					['@babel/preset-env', {
-						targets: '> 0.25%, not dead'
-					}]
-				],
-				plugins: [
-					'@babel/plugin-syntax-dynamic-import',
-					['@babel/plugin-transform-runtime', {
-						useESModules: true
-					}]
-				]
-			}),
+			legacy &&
+				babel({
+					extensions: [".js", ".mjs", ".html", ...extensions],
+					babelHelpers: "runtime",
+					exclude: ["node_modules/@babel/**"],
+					presets: [
+						[
+							"@babel/preset-env",
+							{
+								targets: "> 0.25%, not dead"
+							}
+						]
+					],
+					plugins: [
+						"@babel/plugin-syntax-dynamic-import",
+						[
+							"@babel/plugin-transform-runtime",
+							{
+								useESModules: true
+							}
+						]
+					]
+				}),
 
-			!dev && terser({
-				module: true
-			})
+			!dev &&
+				terser({
+					module: true
+				})
 		],
 
-		onwarn,
+		onwarn
 	},
 
 	server: {
 		input: config.server.input(),
 		output: config.server.output(),
 		plugins: [
+		glob(),
 			replace({
-				'process.browser': false,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				"process.browser": false,
+				"process.env.NODE_ENV": JSON.stringify(mode)
 			}),
 			svelte({
-				generate: 'ssr',
+                extensions,
+                hydratable: true,
+				preprocess: mdsvex(),
+				generate: "ssr",
 				dev
 			}),
-			resolve(),
+			resolve({
+				dedupe: ['svelte']
+			}),
 			commonjs()
 		],
-		external: Object.keys(pkg.dependencies).concat(
-			require('module').builtinModules || Object.keys(process.binding('natives'))
-		),
-
-		onwarn,
+		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
+		preserveEntrySignatures: 'strict',
+		onwarn
 	},
 
 	serviceworker: {
@@ -85,13 +110,13 @@ export default {
 		plugins: [
 			resolve(),
 			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				"process.browser": true,
+				"process.env.NODE_ENV": JSON.stringify(mode)
 			}),
 			commonjs(),
 			!dev && terser()
-		],
-
-		onwarn,
+        ],
+        preserveEntrySignatures: false,
+		onwarn
 	}
 };
