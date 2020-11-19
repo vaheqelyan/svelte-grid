c0358664aedb2c198ee4d4c60b40944405dc9662
Vahe Araqelyan <vahe.araqelyan.2013@hotmail.com>
2020-11-19 20:56:13 +0400

    chore: reformat source

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,8 @@ import pkg from "./package.json";
 
 const name = pkg.name
   .replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
-  .replace(/^\w/, m => m.toUpperCase())
-  .replace(/-\w/g, m => m[1].toUpperCase());
+  .replace(/^\w/, (m) => m.toUpperCase())
+  .replace(/-\w/g, (m) => m[1].toUpperCase());
 
 export default [
   {
