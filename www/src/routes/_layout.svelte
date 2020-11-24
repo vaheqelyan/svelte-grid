69d0f4ec2e29fac79a526da5a6fe9a5617597fe6
Vahe Araqelyan <vahe.araqelyan.2013@hotmail.com>
2020-11-24 22:23:34 +0400

    style(www): create container

--- www/src/routes/_layout.svelte
+++ www/src/routes/_layout.svelte
@@ -1,21 +1,27 @@
 <style>
-  :global(#sapper) {
-    display: flex;
-    min-height: 100vh;
-    flex-direction: column;
-  }
-  .content {
-    flex: 1;
-    padding: 4% 7%;
-  }
+:global(#sapper) {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+.content {
+  flex: 1;
+}
+.container {
+  max-width: 1100px;
+  width: 100%;
+  margin: 0 auto;
+}
 </style>
 
 <Ribbon />
 
 <Nav {segment} />
 
-<div class="content">
-  <slot />
+<div class=content>
+  <div class=container>
+    <slot />
+  </div>
 </div>
 
 <Footer />
