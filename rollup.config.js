75eb6f7123bc48e6229fc1a14892760f83071517
Vahe Araqelyan <vahe.araqelyan.2013@hotmail.com>
2020-11-28 00:18:24 +0400

    chore: set up babel and svelte-preprocess

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
 import pkg from "./package.json";
+import babel from "rollup-plugin-babel";
+import sveltePreprocess from "svelte-preprocess";
 
 const name = pkg.name
   .replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
@@ -14,7 +16,7 @@ export default [
       { file: pkg.module, format: "es" },
       { file: pkg.main, format: "umd", name },
     ],
-    plugins: [svelte(), resolve()],
+    plugins: [svelte({ dev: true, preprocess: sveltePreprocess({ babel: true }) }), resolve()],
   },
   {
     input: "./src/utils/helper.js",
@@ -22,5 +24,6 @@ export default [
       { file: "build/helper/index.mjs", format: "es" },
       { file: "build/helper/index.js", format: "umd", name: "gridHelper" },
     ],
+    plugins: [babel()],
   },
 ];
