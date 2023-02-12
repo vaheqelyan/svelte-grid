d48c3d6853037d604d1f3a7668dee293b757eba2
Vahe Araqelyan <vahe.araqelyan.2013@hotmail.com>
2023-02-12 19:11:04 +0400

    feat: Update packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,38 @@
-import svelte from "rollup-plugin-svelte";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import pkg from "./package.json";
+import svelte from 'rollup-plugin-svelte'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import pkg from './package.json'
+import autoPreprocess from 'svelte-preprocess'
+import typescript from '@rollup/plugin-typescript'
 
 const name = pkg.name
-  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
+  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
   .replace(/^\w/, (m) => m.toUpperCase())
-  .replace(/-\w/g, (m) => m[1].toUpperCase());
+  .replace(/-\w/g, (m) => m[1].toUpperCase())
+
+const production = process.env.NODE_ENV !== 'development'
 
 export default [
   {
-    input: "src/index.svelte",
+    input: 'src/index.svelte',
     output: [
-      { file: pkg.module, format: "es" },
-      { file: pkg.main, format: "umd", name },
+      { file: pkg.module, format: 'es' },
+      { file: pkg.main, format: 'umd', name },
     ],
-    plugins: [svelte(), resolve(), commonjs()],
+    plugins: [
+      svelte({
+        preprocess: autoPreprocess(),
+      }),
+      resolve(),
+      commonjs(),
+      typescript({ sourceMap: !production }),
+    ],
   },
   {
-    input: "./src/utils/helper.js",
+    input: './src/utils/helper.js',
     output: [
-      { file: "build/helper/index.mjs", format: "es" },
-      { file: "build/helper/index.js", format: "umd", name: "gridHelper" },
+      { file: 'build/helper/index.mjs', format: 'es' },
+      { file: 'build/helper/index.js', format: 'umd', name: 'gridHelper' },
     ],
   },
-];
+]
